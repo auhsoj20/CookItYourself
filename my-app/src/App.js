b5d9e1682036ee9d930866e5c05bc7222ac82f63
auhsoj20 <auhsoj20lp@gmx.de>
2022-08-19 20:44:27 +0200

    Image & Text auf Indexseite

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Cooking.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <a>
+          Jetzt das Projekt auf &nbsp;
+          <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/auhsoj20/CookItYourself/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github 
         </a>
+        &nbsp; verfolgen!
+        </a>
+        
       </header>
     </div>
   );
