b8ae6a58bc5bd507fd4a157c8d15d12a6df39315
joshua.st_ <56455953+auhsoj20@users.noreply.github.com>
2023-09-06 12:59:41 +0200

    test commit and

--- BackEnd-Python/main.py
+++ BackEnd-Python/main.py
@@ -8,6 +8,10 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/test")
+def read_root():
+    return {"TEST"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
